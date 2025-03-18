04a05791bde92f5171aac39606838b6a37e1ccda
mokuri kondo <mame.6757@gmail.com>
2025-03-18 14:48:55 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,css}",
+    // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,css}",
+    "./src/app/**/*.{js,ts,jsx,css}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
